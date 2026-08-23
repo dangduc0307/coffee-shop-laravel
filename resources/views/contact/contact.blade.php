b18a6de22aaa8853466f0d5bf5099e1a1fb674bf
your name <duc.d.62cntt@ntu.edu.vn>
2026-08-23 21:47:33 +0700

    Create Contact

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -4,27 +4,7 @@
 
 @section('content')
 
-@if(session('success'))
 
-    <div class="container mt-4">
-
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-
-            <i class="bi bi-check-circle me-2"></i>
-
-            {{ session('success') }}
-
-            <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="alert"
-            ></button>
-
-        </div>
-
-    </div>
-
-@endif
 
 
 
@@ -182,6 +162,27 @@
 
             {{-- CONTACT FORM --}}
             <div class="col-12 col-lg-7">
+                @if(session('success'))
+
+                    <div class="container mt-4">
+
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+
+                            <i class="bi bi-check-circle me-2"></i>
+
+                            {{ session('success') }}
+
+                            <button
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="alert"
+                            ></button>
+
+                        </div>
+
+                    </div>
+
+                @endif
 
                 <div class="contact-form">
 
